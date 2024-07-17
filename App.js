738aaab54ef85de31130dc42bfb9f9fb2ba455de
LTSGOD <lts0424@naver.com>
2024-07-17 12:17:09 +0900

    feat: 기존 기능 추가
    - gitignore 변경

---
+++ App.js
@@ -0,0 +1,132 @@
+//App.js
+
+import React, { useEffect } from 'react';
+import NotificationScreen from './NotificationScreen';
+import ScheduleScreen from './ScheduleScreen.js'
+import imgUpload from './imgupload.js'
+ import messaging from '@react-native-firebase/messaging';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {Button} from "react-native";
+
+ import type { PropsWithChildren } from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+
+ type SectionProps = PropsWithChildren<{
+   title: string;
+ }>;
+
+ messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+   console.log('[Background Remote Message]', remoteMessage);
+ });
+
+const App = () => {
+    const Stack = createStackNavigator();
+   useEffect(() => {
+     getFcmToken();
+   }, []);
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Start">
+                <Stack.Screen name="Start" component={ScheduleScreen} />
+                <Stack.Screen name="NotificationScreen" component={NotificationScreen} />
+                <Stack.Screen name="imgUpload" component={imgUpload} />
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    );
+};
+
+function Start({navigation}) {
+  return (
+    <View>
+      <Text>start!</Text>
+      <Button
+        title="go to Notification"
+        onPress={() => navigation.navigate('NotificationScreen')}
+      />
+
+      <Button
+              title="go to ScheduleScreen"
+              onPress={() => navigation.navigate('ScheduleScreen')}
+            />
+    </View>
+  );
+}
+
+
+ const getFcmToken = async () => {
+   const fcmToken = await messaging().getToken();
+   console.log('[FCM Token] ', fcmToken);
+ };
+
+ function Section({ children, title }: SectionProps): React.JSX.Element {
+   const isDarkMode = useColorScheme() === 'dark';
+   return (
+     <View style={styles.sectionContainer}>
+       <Text
+         style={[
+           styles.sectionTitle,
+           {
+             color: isDarkMode ? Colors.white : Colors.black,
+           },
+         ]}
+       >
+         {title}
+       </Text>
+       <Text
+         style={[
+           styles.sectionDescription,
+           {
+             color: isDarkMode ? Colors.light : Colors.dark,
+           },
+         ]}
+       >
+         {children}
+       </Text>
+     </View>
+   );
+ }
+
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     alignItems: 'center',
+     justifyContent: 'center',
+   },
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+ });
+
+ export default App;
