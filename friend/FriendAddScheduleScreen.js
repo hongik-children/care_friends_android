c713bf17d7b530de6660702a5add3816a875e6f4
ngj1129 <grw13141@naver.com>
2024-11-14 13:46:03 +0900

    refactor: 일정 추가 화면 날짜 선택, 시간 선택 ui 수정

--- friend/FriendAddScheduleScreen.js
+++ friend/FriendAddScheduleScreen.js
@@ -1,50 +1,45 @@
 import React, { useState } from 'react';
-import { View, TextInput, Alert, Pressable, Text, ScrollView, StyleSheet, Platform } from 'react-native';
+import { View, TextInput, Alert, TouchableOpacity, Text, ScrollView, StyleSheet } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import axios from 'axios';
 import { Picker } from '@react-native-picker/picker';
-import AsyncStorage from '@react-native-async-storage/async-storage'; // JWT 토큰을 가져오기 위해 추가
-import { BASE_URL } from '@env'; // @env 모듈로 불러옴
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { BASE_URL } from '@env';
 import CustomText from '../CustomTextProps';
 
 const FriendAddScheduleScreen = ({ navigation }) => {
   const [date, setDate] = useState(new Date());
+  const [startTime, setStartTime] = useState(new Date());
   const [periodType, setPeriodType] = useState('NONE');
   const [period, setPeriod] = useState('');
-  const [startTime, setStartTime] = useState(new Date());
   const [title, setTitle] = useState('');
   const [location, setLocation] = useState('');
   const [memo, setMemo] = useState('');
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showTimePicker, setShowTimePicker] = useState(false);
 
-  // 일정 저장 함수
   const handleSave = async () => {
     const taskRequest = {
-      date: date.toISOString().split('T')[0], // YYYY-MM-DD 형식으로 변환
+      date: date.toISOString().split('T')[0],
       periodType,
       period,
-      startTime: new Date(startTime.getTime() + 9 * 60 * 60 * 1000).toISOString().split('T')[1].substring(0, 8),
+      startTime: startTime.toTimeString().split(' ')[0],
       title,
       location,
       memo,
     };
 
     try {
-      // JWT 토큰 가져오기
       const jwtToken = await AsyncStorage.getItem('jwtToken');
       if (!jwtToken) {
         Alert.alert("오류", "JWT 토큰을 찾을 수 없습니다. 다시 로그인하세요.");
         return;
       }
 
-      console.log(BASE_URL); //BASE_URL이 안불러와지는 에러 해결
-      const apiUrl = `${BASE_URL}/task`; // API URL
-
-      // 서버로 요청 보내기
+      const apiUrl = `${BASE_URL}/task`;
       const response = await axios.post(apiUrl, taskRequest, {
         headers: {
-          Authorization: `Bearer ${jwtToken}` // JWT 토큰을 헤더에 추가
+          Authorization: `Bearer ${jwtToken}`
         }
       });
 
@@ -73,13 +68,12 @@ const FriendAddScheduleScreen = ({ navigation }) => {
 
   return (
     <ScrollView style={styles.container}>
-      <CustomText style={{ color: '#333', marginBottom: 10, marginHorizontal: 8 }}>주기</CustomText>
-      <View style={{ borderWidth: 1, borderColor: '#333', marginBottom: 20, overflow: 'visible' }}>
+      <CustomText style={styles.label}>주기</CustomText>
+      <View style={styles.pickerContainer}>
         <Picker
           selectedValue={periodType}
           onValueChange={(itemValue) => setPeriodType(itemValue)}
           style={styles.picker}
-          dropdownIconColor={'#6495ED'}
         >
           <Picker.Item label="없음" value="NONE" />
           <Picker.Item label="매일" value="DAY" />
@@ -94,7 +88,6 @@ const FriendAddScheduleScreen = ({ navigation }) => {
         value={period}
         onChangeText={setPeriod}
         style={styles.input}
-        placeholderTextColor={'#333'}
       />
 
       <TextInput
@@ -102,7 +95,6 @@ const FriendAddScheduleScreen = ({ navigation }) => {
         value={title}
         onChangeText={setTitle}
         style={styles.input}
-        placeholderTextColor={'#333'}
       />
 
       <TextInput
@@ -110,7 +102,6 @@ const FriendAddScheduleScreen = ({ navigation }) => {
         value={location}
         onChangeText={setLocation}
         style={styles.input}
-        placeholderTextColor={'#333'}
       />
 
       <TextInput
@@ -119,24 +110,12 @@ const FriendAddScheduleScreen = ({ navigation }) => {
         onChangeText={setMemo}
         style={styles.input}
         multiline
-        placeholderTextColor={'#333'}
       />
 
-      <View style={styles.dateTimePicker}>
-        <Pressable
-          style={{ borderRadius: 6, width: 120, height: 48, backgroundColor: '#6495ED', alignItems: 'center', justifyContent: 'center' }}
-          onPress={() => setShowDatePicker(true)} color="#6495ED"
-        >
-          <CustomText style={{ color: '#FFF', fontSize: 16}}>날짜 선택</CustomText>
-        </Pressable>
-        <Pressable
-          style={{ borderRadius: 6, width: 120, height: 48, backgroundColor: '#6495ED', alignItems: 'center', justifyContent: 'center' }}
-          onPress={() => setShowTimePicker(true)} color="#6495ED"
-        >
-          <CustomText style={{ color: '#FFF', fontSize: 16 }}>시간 선택</CustomText>
-        </Pressable>
-      </View>
-
+      <Text style={styles.label}>방문 예정 날짜</Text>
+      <TouchableOpacity style={styles.dateButton} onPress={() => setShowDatePicker(true)}>
+        <Text style={styles.dateButtonText}>{date.toLocaleDateString()}</Text>
+      </TouchableOpacity>
       {showDatePicker && (
         <DateTimePicker
           value={date}
@@ -146,6 +125,10 @@ const FriendAddScheduleScreen = ({ navigation }) => {
         />
       )}
 
+      <Text style={styles.label}>방문 예정 시간</Text>
+      <TouchableOpacity style={styles.dateButton} onPress={() => setShowTimePicker(true)}>
+        <Text style={styles.dateButtonText}>{startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</Text>
+      </TouchableOpacity>
       {showTimePicker && (
         <DateTimePicker
           value={startTime}
@@ -155,12 +138,9 @@ const FriendAddScheduleScreen = ({ navigation }) => {
         />
       )}
 
-      <Pressable
-        style={{ borderRadius: 6, height: 48, backgroundColor: '#6495ED', alignItems: 'center', justifyContent: 'center' }}
-        onPress={handleSave}
-      >
-        <CustomText style={{ color: '#FFF', fontSize: 17 }}>저장</CustomText>
-      </Pressable>
+      <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+        <Text style={styles.saveButtonText}>저장</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
@@ -171,6 +151,21 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#FFFFFF',
   },
+  label: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 8,
+  },
+  pickerContainer: {
+    borderWidth: 1,
+    borderColor: '#333',
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  picker: {
+    backgroundColor: '#FFF',
+    color: '#333',
+  },
   input: {
     height: 50,
     borderColor: '#333',
@@ -180,14 +175,28 @@ const styles = StyleSheet.create({
     color: '#000',
     borderRadius: 10,
   },
-  picker: {
-    backgroundColor: '#FFF',
+  dateButton: {
+    backgroundColor: '#E8EAF6',
+    paddingVertical: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  dateButtonText: {
+    fontSize: 16,
     color: '#333',
   },
-  dateTimePicker: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
+  saveButton: {
+    backgroundColor: '#6495ED',
+    paddingVertical: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  saveButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
@@ -197,14 +206,15 @@ export default FriendAddScheduleScreen;
 
 
 //import React, { useState } from 'react';
-//import { View, TextInput, Button, StyleSheet, Platform, Alert, Pressable, Text, ScrollView } from 'react-native';
+//import { View, TextInput, Alert, Pressable, Text, ScrollView, StyleSheet, Platform } from 'react-native';
 //import DateTimePicker from '@react-native-community/datetimepicker';
 //import axios from 'axios';
 //import { Picker } from '@react-native-picker/picker';
+//import AsyncStorage from '@react-native-async-storage/async-storage'; // JWT 토큰을 가져오기 위해 추가
 //import { BASE_URL } from '@env'; // @env 모듈로 불러옴
+//import CustomText from '../CustomTextProps';
 //
-//const AddScheduleScreen = ({ navigation }) => {
-//  const [friendId, setFriendId] = useState('');
+//const FriendAddScheduleScreen = ({ navigation }) => {
 //  const [date, setDate] = useState(new Date());
 //  const [periodType, setPeriodType] = useState('NONE');
 //  const [period, setPeriod] = useState('');
@@ -215,22 +225,36 @@ export default FriendAddScheduleScreen;
 //  const [showDatePicker, setShowDatePicker] = useState(false);
 //  const [showTimePicker, setShowTimePicker] = useState(false);
 //
+//  // 일정 저장 함수
 //  const handleSave = async () => {
 //    const taskRequest = {
-//      friendId,
-//      date: date.toISOString().split('T')[0],
+//      date: date.toISOString().split('T')[0], // YYYY-MM-DD 형식으로 변환
 //      periodType,
 //      period,
-//      startTime: startTime.toISOString().split('T')[1].substring(0, 8),
+//      startTime: new Date(startTime.getTime() + 9 * 60 * 60 * 1000).toISOString().split('T')[1].substring(0, 8),
 //      title,
 //      location,
 //      memo,
 //    };
 //
 //    try {
-//      const apiUrl = Platform.OS === 'android' ? `${BASE_URL}/task` : `${BASE_URL}/task`;
+//      // JWT 토큰 가져오기
+//      const jwtToken = await AsyncStorage.getItem('jwtToken');
+//      if (!jwtToken) {
+//        Alert.alert("오류", "JWT 토큰을 찾을 수 없습니다. 다시 로그인하세요.");
+//        return;
+//      }
 //
-//      const response = await axios.post(apiUrl, taskRequest);
+//      console.log(BASE_URL); //BASE_URL이 안불러와지는 에러 해결
+//      const apiUrl = `${BASE_URL}/task`; // API URL
+//
+//      // 서버로 요청 보내기
+//      const response = await axios.post(apiUrl, taskRequest, {
+//        headers: {
+//          Authorization: `Bearer ${jwtToken}` // JWT 토큰을 헤더에 추가
+//        }
+//      });
+//
 //      console.log('Task saved:', response.data);
 //      Alert.alert("성공", "일정이 저장되었습니다.");
 //      navigation.goBack();
@@ -256,71 +280,70 @@ export default FriendAddScheduleScreen;
 //
 //  return (
 //    <ScrollView style={styles.container}>
-//      <TextInput
-//        placeholder="친구 ID"
-//        value={friendId}
-//        onChangeText={setFriendId}
-//        style={[styles.input, {marginBottom:16}]}
-//        placeholderTextColor={'#6495ED'}
-//      />
-//      <CustomText style={{color:'#6495ED', marginBottom:10, marginHorizontal:8}}>주기</CustomText>
-//      <View style={{borderWidth:1, borderColor:'#6495ED',
-//    marginBottom: 20, overflow:'visible'}}>
-//      <Picker
-//        selectedValue={periodType}
-//        onValueChange={(itemValue) => setPeriodType(itemValue)}
-//        style={styles.picker}
-//        dropdownIconColor={'#6495ED'}
-//      >
-//        <Picker.Item label="없음" value="NONE" />
-//        <Picker.Item label="매일" value="DAY" />
-//        <Picker.Item label="매주" value="WEEK" />
-//        <Picker.Item label="매월" value="MONTH" />
-//        <Picker.Item label="매년" value="YEAR" />
-//      </Picker>
+//      <CustomText style={{ color: '#333', marginBottom: 10, marginHorizontal: 8 }}>주기</CustomText>
+//      <View style={{ borderWidth: 1, borderColor: '#333', marginBottom: 20, overflow: 'visible' }}>
+//        <Picker
+//          selectedValue={periodType}
+//          onValueChange={(itemValue) => setPeriodType(itemValue)}
+//          style={styles.picker}
+//          dropdownIconColor={'#6495ED'}
+//        >
+//          <Picker.Item label="없음" value="NONE" />
+//          <Picker.Item label="매일" value="DAY" />
+//          <Picker.Item label="매주" value="WEEK" />
+//          <Picker.Item label="매월" value="MONTH" />
+//          <Picker.Item label="매년" value="YEAR" />
+//        </Picker>
 //      </View>
+//
 //      <TextInput
 //        placeholder="반복 주기"
 //        value={period}
 //        onChangeText={setPeriod}
 //        style={styles.input}
-//
-//        placeholderTextColor={'#6495ED'}
+//        placeholderTextColor={'#333'}
 //      />
+//
 //      <TextInput
 //        placeholder="제목"
 //        value={title}
 //        onChangeText={setTitle}
 //        style={styles.input}
-//        placeholderTextColor={'#6495ED'}
+//        placeholderTextColor={'#333'}
 //      />
+//
 //      <TextInput
 //        placeholder="위치"
 //        value={location}
 //        onChangeText={setLocation}
 //        style={styles.input}
-//        placeholderTextColor={'#6495ED'}
+//        placeholderTextColor={'#333'}
 //      />
+//
 //      <TextInput
 //        placeholder="메모"
 //        value={memo}
 //        onChangeText={setMemo}
 //        style={styles.input}
 //        multiline
-//        placeholderTextColor={'#6495ED'}
+//        placeholderTextColor={'#333'}
 //      />
+//
 //      <View style={styles.dateTimePicker}>
 //        <Pressable
-//        style={{borderRadius:6, width:120, height:48, backgroundColor:'#6495ED', alignItems:'center', justifyContent:'center'}}
-//        onPress={() => setShowDatePicker(true)} color="#6495ED">
-//            <CustomText style={{color:'#FFF', fontSize:14 }}>날짜 선택</CustomText>
+//          style={{ borderRadius: 6, width: 120, height: 48, backgroundColor: '#6495ED', alignItems: 'center', justifyContent: 'center' }}
+//          onPress={() => setShowDatePicker(true)} color="#6495ED"
+//        >
+//          <CustomText style={{ color: '#FFF', fontSize: 16}}>날짜 선택</CustomText>
 //        </Pressable>
 //        <Pressable
-//                style={{borderRadius:6, width:120, height:48, backgroundColor:'#6495ED', alignItems:'center', justifyContent:'center'}}
-//                onPress={() => setShowTimePicker(true)} color="#6495ED">
-//                    <CustomText style={{color:'#FFF', fontSize:14 }}>시간 선택</CustomText>
-//                </Pressable>
+//          style={{ borderRadius: 6, width: 120, height: 48, backgroundColor: '#6495ED', alignItems: 'center', justifyContent: 'center' }}
+//          onPress={() => setShowTimePicker(true)} color="#6495ED"
+//        >
+//          <CustomText style={{ color: '#FFF', fontSize: 16 }}>시간 선택</CustomText>
+//        </Pressable>
 //      </View>
+//
 //      {showDatePicker && (
 //        <DateTimePicker
 //          value={date}
@@ -329,6 +352,7 @@ export default FriendAddScheduleScreen;
 //          onChange={onDateChange}
 //        />
 //      )}
+//
 //      {showTimePicker && (
 //        <DateTimePicker
 //          value={startTime}
@@ -337,11 +361,13 @@ export default FriendAddScheduleScreen;
 //          onChange={onTimeChange}
 //        />
 //      )}
+//
 //      <Pressable
-//                      style={{borderRadius:6, height:48, backgroundColor:'#6495ED', alignItems:'center', justifyContent:'center'}}
-//                      onPress={handleSave}>
-//                          <CustomText style={{color:'#FFF', fontSize:14 }}>저장</CustomText>
-//                      </Pressable>
+//        style={{ borderRadius: 6, height: 48, backgroundColor: '#6495ED', alignItems: 'center', justifyContent: 'center' }}
+//        onPress={handleSave}
+//      >
+//        <CustomText style={{ color: '#FFF', fontSize: 17 }}>저장</CustomText>
+//      </Pressable>
 //    </ScrollView>
 //  );
 //};
@@ -354,16 +380,16 @@ export default FriendAddScheduleScreen;
 //  },
 //  input: {
 //    height: 50,
-//    borderColor: '#6495ED',
+//    borderColor: '#333',
 //    borderWidth: 1,
 //    marginBottom: 20,
 //    paddingHorizontal: 16,
-//    color:'#000',
-//    borderRadius:10,
+//    color: '#000',
+//    borderRadius: 10,
 //  },
 //  picker: {
-//    backgroundColor:'#FFF',
-//    color:'#6495ED'
+//    backgroundColor: '#FFF',
+//    color: '#333',
 //  },
 //  dateTimePicker: {
 //    flexDirection: 'row',
@@ -372,5 +398,4 @@ export default FriendAddScheduleScreen;
 //  },
 //});
 //
-//export default AddScheduleScreen;
-
+//export default FriendAddScheduleScreen;
